42206803fee06221404ae0920feb74a163a551ef
gasxchenzho <77746930+gasxchenzho@users.noreply.github.com>
2021-05-04 16:10:28 -0400

    updated UI

--- client/src/components/FlightSearchRow.js
+++ client/src/components/FlightSearchRow.js
@@ -6,40 +6,30 @@ import "../style/FlightSearch.css";
 export default class FlightSearchRow_NONSTOP extends React.Component {
   /* ---- Q2 (Recommendations) ---- */
   render() {
-    return (
-      <div className="flex-row-container">
+    const renderAirportSwitch = () =>{
+      if (this.props.mid_airport == null){
+        return(
+        <div className="flex-row-container">
         <div className="flex-row-item">{this.props.source_airport}</div>
         <div className="flex-row-item">{this.props.dest_airport}</div>
         <div className="flex-row-item">{this.props.time}</div>
         <div className="flex-row-item">{this.props.airlineid}</div>
-        <hr class="blackline" />
-      </div>
-    );
-  }
-}
-
-export class FlightSearchRow_ONESTOP extends React.Component {
-  /* ---- Q2 (Recommendations) ---- */
-  render() {
-    return (
-      <div className="flex-row-container">
-        <div className="flex-row-item">{this.props.source_airport}</div>
-        <div className="flex-row-item">{this.props.mid_airport}</div>
-        <div className="flex-row-item">{this.props.dest_airport}</div>
-        <div className="flex-row-item">{this.props.time}</div>
-        <div className="flex-row-item">{this.props.airlineid_1}</div>
-        <div className="flex-row-item">{this.props.airlineid_2}</div>
         <hr class="solid" />
-
-      </div>
-    );
-  }
-}
-export class FlightSearchRow_TWOSTOP extends React.Component {
-  /* ---- Q2 (Recommendations) ---- */
-  render() {
-    return (
-      <div className="flex-row-container">
+        </div>);
+      }else if (this.props.mid_airport != null){
+        return(
+        <div className="flex-row-container">
+         <div className="flex-row-item">{this.props.source_airport}</div>
+         <div className="flex-row-item">{this.props.mid_airport}</div>
+         <div className="flex-row-item">{this.props.dest_airport}</div>
+         <div className="flex-row-item">{this.props.time}</div>
+         <div className="flex-row-item">{this.props.airlineid_1}</div>
+         <div className="flex-row-item">{this.props.airlineid_2}</div>
+         <hr class="solid" />
+       </div>);
+      }else if(this.props.mid_airport_1 != null){
+        return(
+        <div className="flex-row-container">
         <div className="flex-row-item">{this.props.source_airport}</div>
         <div className="flex-row-item">{this.props.mid_airport_1}</div>
         <div className="flex-row-item">{this.props.mid_airport_2}</div>
@@ -50,7 +40,19 @@ export class FlightSearchRow_TWOSTOP extends React.Component {
         <div className="flex-row-item">{this.props.airlineid_3}</div>
         <hr class="blackline" />
 
-      </div>
-    );
+        </div>);
+      }
+    }
+  return(
+    {renderAirportSwitch()}
+
+  );
   }
+  
 }
+
+
+     
+
+
+
